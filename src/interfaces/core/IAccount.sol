f4f3ee97d333607d8c12d51c36625ab1a69121ca
insecureMary <maryodior123@gmail.com>
2026-01-29 15:55:24 +0100

    fixed imports in accountmanager

--- src/interfaces/core/IAccount.sol
+++ src/interfaces/core/IAccount.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
-interface IAccount {}
+import {IManager} from "./IManager.sol";
+
+/**
+ * @title IAccount
+ * @notice Interface for the Account contract.
+ */
+interface IAccount {
+    //ERRORS
+    error IAccount__ZeroAddressInput();
+    error IAccount__FailedToApprove();
+
+    //VIEW FUNCTIONS
+    function manager() external view returns (IManager);
+
+    //STATE-CHANGING FUNCTIONS
+    /**
+     * @notice Approves a spender to spend a specified amount of a token on behalf of the account.
+     * @param _token The address of the token to approve.
+     * @param _spender The address of the spender to approve.
+     * @param _amount The amount of the token to approve.
+     */
+    function approve(address _token, address _spender, uint256 _amount) external;
+    function transfer(address _token, address _to, uint256 _amount) external;
+}
